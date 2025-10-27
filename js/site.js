120a50a4bd1c6a7825c742c07012f798fd10a604
DiegoR003 <diegomossonava248@gmail.com>
2025-10-27 16:47:25 -0700

    cambios temporales

--- js/site.js
+++ js/site.js
@@ -12,3 +12,26 @@
   onScroll();
   window.addEventListener('scroll', onScroll);
 })();
+
+// Carrusel de partners (Slick)
+$(function(){
+  if ($('.partners-slider').length){
+    $('.partners-slider').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2200,
+      speed: 400,
+      arrows: true,
+      dots: false,
+      pauseOnHover: false,
+      cssEase: 'ease',
+      responsive: [
+        { breakpoint: 1200, settings: { slidesToShow: 5 } },
+        { breakpoint: 992,  settings: { slidesToShow: 4 } },
+        { breakpoint: 768,  settings: { slidesToShow: 3 } },
+        { breakpoint: 576,  settings: { slidesToShow: 2 } }
+      ]
+    });
+  }
+});
